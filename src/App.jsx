aa4030257506c6a566aa31c6cc93f1e76c9b335c
destbreak <rey.hadiansyah7th@gmail.com>
2024-06-06 19:32:33 +0700

    develop delete todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,17 @@ function App() {
     setTodos(updatedTodos)
   }
   
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => {
+      return todo.id !== todoId
+    })
+    setTodos(updatedTodos)
+  }
+  
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
     </div>
   )
 }
